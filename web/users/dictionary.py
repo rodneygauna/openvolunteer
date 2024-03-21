074724822191445155be8b9934a3b02fa9b11401
Rodney Gauna <rodneygauna@gmail.com>
2024-03-21 11:27:50 -0700

    starting module dictionaries

---
+++ web/users/dictionary.py
@@ -0,0 +1,15 @@
+"""This module contains the dictionary of users."""
+
+# User Roles
+USER_ROLES = [
+    ("admin", "Admin"),
+    ("super user", "Super User"),
+    ("user", "User"),
+]
+
+# User Status
+USER_STATUS = [
+    ("active", "Active"),
+    ("inactive", "Inactive"),
+    ("suspended", "Suspended"),
+]
