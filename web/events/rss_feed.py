b79252551b097772ab32e57c052a2fdad974b841
Rodney Gauna <rodneygauna@gmail.com>
2024-03-26 14:58:15 -0700

    bye bye rss (no longer valid in the market)

--- web/events/rss_feed.py
+++ web/events/rss_feed.py
@@ -1,7 +1,6 @@
 """Create an RSS feed for events."""
 # Imports
 from datetime import datetime
-import xml.etree.ElementTree as ET
 import pytz
 from ics import Calendar, Event as IcsEvent
 from flask import Blueprint, Response
@@ -12,59 +11,6 @@ from .models import Event
 
 # Blueprint
 rss_feed_bp = Blueprint('rss_feed', __name__)
-
-
-# Routes and Views
-@rss_feed_bp.route('/events/rss')
-def events_feed():
-    """Create an RSS feed for events."""
-
-    events = (
-        db.session.query(
-            Event.id,
-            Event.title,
-            Event.description,
-            Event.start_date,
-            Event.start_time,
-            Event.start_timezone,
-            Event.event_status,
-            Event.created_date,
-            Event.updated_date,
-            Event.event_leader_id,
-            User.first_name,
-            User.last_name
-        )
-        .join(User, Event.event_leader_id == User.id)
-        .filter(Event.start_date >= datetime.now())
-        .filter(Event.event_status == 'open')
-        .order_by(Event.start_date.asc())
-        .all()
-    )
-
-    rss = ET.Element('rss', version='2.0')
-    channel = ET.SubElement(rss, 'channel')
-    ET.SubElement(channel, 'title').text = 'OpenVolunteer Events Feed'
-    ET.SubElement(
-        channel, 'description').text = 'Upcoming events for OpenVolunteer'
-    ET.SubElement(channel, 'lastBuildDate').text = datetime.now(
-        pytz.utc).strftime('%a, %d %b %Y %H:%M:%S %z')
-
-    for event in events:
-        item = ET.SubElement(channel, 'item')
-        ET.SubElement(item, 'guid').text = str(event.id)
-        ET.SubElement(item, 'title').text = event.title
-        description_text = f"{event.description}\n\nStart Date: {event.start_date.strftime('%a, %d %b %Y')}\nStart Time: {
-            event.start_time.strftime('%H:%M %p')}\nTimezone: {event.start_timezone}\nCreated By: {event.first_name} {event.last_name}"
-        ET.SubElement(item, 'description').text = description_text
-        pubDate = datetime.combine(event.start_date, event.start_time).astimezone(
-            pytz.timezone(event.start_timezone)).strftime('%a, %d %b %Y %H:%M:%S %Z')
-        ET.SubElement(item, 'pubDate').text = pubDate
-
-    rss_string = ET.tostring(rss, encoding='utf-8', method='xml')
-
-    response = Response(rss_string, mimetype='application/rss+xml')
-
-    return response
 
 
 # calendar.ics route
