c7c085a8a3511399bd2be6968af771bc265bff1c
Rodney Gauna <rodneygauna@gmail.com>
2024-03-28 15:02:43 -0700

    Force password reset is now enabled

--- web/settings/views.py
+++ web/settings/views.py
@@ -1,10 +1,15 @@
 """Views for the settings portion of the application."""
 # Imports
 from datetime import datetime
-from flask import Blueprint, render_template, redirect, url_for
+from random import choice
+from string import ascii_letters, digits
+from flask import Blueprint, render_template, redirect, url_for, flash
+from werkzeug.security import generate_password_hash
 from flask_login import login_required, current_user
+from flask_mail import Message
 from users.admin_superuser import admin_required, superuser_required
-from app import db
+from users.models import User
+from app import db, mail
 from .queries import get_users
 from .forms import OrganizationForm, DefaultPreferenceForm, LocationForm
 from .models import Organization, DefaultPreference, Location
@@ -136,3 +141,42 @@ def view_users():
     """View all users."""
     users = get_users()
     return render_template("settings/users.html", users=users)
+
+
+# Function - Reset Password
+def reset_password(user_id):
+    """Forces the user to reset their password (usually by an admin)"""
+    if current_user.role != 'admin':
+        flash('You are not authorized to reset passwords.', 'danger')
+        return redirect(url_for('settings.view_users'))
+    if current_user.id == user_id:
+        flash('You cannot reset your own password.', 'danger')
+        return redirect(url_for('settings.view_users'))
+    else:
+        user = User.query.get_or_404(user_id)
+        # Generate a random password of 12 alphanumeric characters
+        new_password = ''.join(
+            (choice(ascii_letters + digits) for i in range(12)))
+        user.password_hash = generate_password_hash(new_password)
+        db.session.commit()
+        flash('The user\'s password has been reset.', 'success')
+        # Send the new password to the user's email
+        msg = Message(
+            'OpenVolunteer - Short Code for Login',
+            recipients=[user.email],
+            sender="noreply-2FA@openvolunteer.com")
+        msg.body = f"""
+Your password has been reset by the administrator.
+Please login with the following password: {new_password}
+        """
+        mail.send(msg)
+
+
+# Route - Reset Password
+@settings_bp.route('/reset_password/<int:user_id>', methods=['POST'])
+@login_required
+@admin_required
+def reset_password_route(user_id):
+    """Routes the admin to reset a user's password"""
+    reset_password(user_id)
+    return redirect(url_for('settings.view_users'))
