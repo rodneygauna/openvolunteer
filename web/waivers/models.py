1a99c167f5b4bc4b3b10555fce50bcdc216c30af
Rodney Gauna <rodneygauna@gmail.com>
2024-04-18 11:00:30 -0700

    waiver model started

---
+++ web/waivers/models.py
@@ -0,0 +1,34 @@
+"""Database models for the waivers feature of the application."""
+# Imports
+from datetime import datetime
+from app import db
+
+
+# Model - Waiver
+class Waiver(db.Model):
+    """Waiver model"""
+
+    __tablename__ = "waivers"
+
+    id = db.Column(db.Integer, primary_key=True)
+    active_date = db.Column(db.Date, nullable=False)
+    expiration_date = db.Column(db.Date)
+    version = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    created_date = db.Column(db.DateTime, default=datetime.utcnow())
+    created_by = db.Column(
+        db.Integer, db.ForeignKey("users.id"), nullable=False)
+    updated_date = db.Column(db.DateTime)
+    updated_by = db.Column(db.Integer, db.ForeignKey("users.id"))
+
+
+# Model - Waiver Agreement
+class WaiverAgreement(db.Model):
+    """Waiver Agreement model"""
+
+    __tablename__ = "waiver_agreements"
+
+    id = db.Column(db.Integer, primary_key=True)
+    waiver_id = db.Column(db.Integer, db.ForeignKey("waivers.id"))
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    agreement_date = db.Column(db.DateTime, default=datetime.utcnow())
